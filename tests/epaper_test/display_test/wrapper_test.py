39fe07cefc1176cf10148e95fd253e2b3c924251
Thomas Nelson <tanelso2@gmail.com>
2026-05-09 22:36:15 -0500

    Add a fixture to write tests against the mock display and the real one

--- tests/epaper_test/display_test/wrapper_test.py
+++ tests/epaper_test/display_test/wrapper_test.py
@@ -1,11 +1,40 @@
 import pytest
 
-from epaper.display.wrapper import get_display_wrapper
+from epaper.display.wrapper import AsyncEPDWrapper, get_display_wrapper
 
 
-@pytest.mark.asyncio
-async def test_mock_display_wrapper():
-    e = get_display_wrapper(use_mock=True)
+@pytest.fixture
+def real_display_wrapper() -> AsyncEPDWrapper:
+    try:
+        e = get_display_wrapper(use_mock=False)
+        return e
+    except Exception:
+        pytest.skip("Could not load real display")
+
+
+@pytest.fixture
+def mock_display_wrapper() -> AsyncEPDWrapper:
+    return get_display_wrapper(use_mock=True)
+
+
+@pytest.fixture(params=[True, False])
+def parameterized_display_wrapper(request) -> AsyncEPDWrapper:
+    use_mock = request.param
+    try:
+        e = get_display_wrapper(use_mock=use_mock)
+        return e
+    except Exception:
+        if use_mock:
+            # Should never fail, propagate failure
+            raise
+        else:
+            pytest.skip("Could not load real display")
+
+
+async def test_display_wrapper(parameterized_display_wrapper: AsyncEPDWrapper):
+    e = parameterized_display_wrapper
     await e.init()
     await e.blank_screen()
+    await e.sleep()
     await e.clear()
+    await e.blank_screen()
